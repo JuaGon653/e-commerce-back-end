45e6158c8d258d4445e2e8d2b54bf8aa135d8e92
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-19 12:56:38 -0500

    added GET request methods

--- models/index.js
+++ models/index.js
@@ -5,12 +5,36 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag
+});
+Tag.belongsToMany(Product, {
+  through: ProductTag
+});
 
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
 
+
+// Tag.hasOne(ProductTag, {
+//   foreignKey: 'tag_id'
+// });
+// ProductTag.belongsTo(Tag, {
+//   foreignKey: 'tag_id'
+// });
+
+
+
 // Tags belongToMany Products (through ProductTag)
+
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,39 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const userData = await Category.findAll({
+      include: [{ model: Product}]
+    });
+    if (!userData) {
+      res.status(400).json({ message: 'No data found.'});
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const catData = await Category.findAll({
+      include: [{ model: Product}],
+      where: {id:req.params.id}
+    });
+    if (!catData) {
+      res.status(400).json({ message: 'No data found with the given ID.'});
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
