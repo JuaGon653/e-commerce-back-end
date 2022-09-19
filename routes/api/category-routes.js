bbefe39815dba4d1f5912e7150052681dce45041
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-19 13:24:17 -0500

    added POST and PUT requests

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,12 +38,40 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const catData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+    try {
+      const updatedCat = await Category.update(
+        {
+          category_name: req.body.category_name
+        },
+        {
+          where: {
+            id: req.params.id
+          }
+        }
+      );
+
+      if(!updatedCat[0]) {
+        res.status(404).json({ message: 'No category with this id or given name is already set!'});
+        return;
+      }
+      res.status(200).json(updatedCat);
+    } catch (err) {
+      res.status(400).json(err);
+    }
 });
 
 router.delete('/:id', (req, res) => {
