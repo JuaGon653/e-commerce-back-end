b3896e764dbf8b4318c9e6b89f57aa63fd872f97
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-20 10:37:15 -0500

    implemented PUT request

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -49,8 +49,28 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updatedTag = await Tag.update(
+      {
+        tag_name: req.body.tag_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+
+    if (!updatedTag[0]) {
+      res.status(404).json({ message: 'No Tag with this id or given name is already set!' });
+    }
+
+    res.status(200).json(updatedTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
